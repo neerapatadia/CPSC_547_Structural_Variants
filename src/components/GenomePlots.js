e6043e17b474591bbf74193dd2e87dc834fb7c8c
Armita <armt.safa@gmail.com>
2021-12-11 17:40:28 -0800

    show matched variants on circos plot

--- src/components/GenomePlots.js
+++ src/components/GenomePlots.js
@@ -10,101 +10,111 @@ const GenomePlots = ({ width }) => {
         static: true,
         layout: "circular",
         alignment: "stack",
-        tracks: [
-          {
-            alignment: "overlay",
-            data: {
-              url: "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec",
-              type: "multivec",
-              row: "sample",
-              column: "position",
-              value: "peak",
-              categories: ["sample 1", "sample 2", "sample 3", "sample 4"],
-              binSize: 4,
-            },
-            x: { field: "start", type: "genomic" },
-            xe: { field: "end", type: "genomic" },
-            y: { field: "peak", type: "quantitative" },
-            row: { field: "sample", type: "nominal" },
-            color: { field: "sample", type: "nominal" },
-            stroke: { value: "black" },
-            strokeWidth: { value: 0.3 },
-            tracks: [
-              { mark: "bar" },
-              {
-                mark: "brush",
-                x: { linkingId: "detail-1" },
-                color: { value: "blue" },
-              },
-              {
-                mark: "brush",
-                x: { linkingId: "detail-2" },
-                color: { value: "red" },
-              },
-            ],
-            style: { outlineWidth: 0 },
-            width,
-            height: 100,
-          },
-          {
-            data: {
-              type: "csv",
-              url: "https://raw.githubusercontent.com/sehilyi/gemini-datasets/master/data/rearrangements.bulk.1639.simple.filtered.pub",
-              headerNames: [
-                "chr1",
-                "p1s",
-                "p1e",
-                "chr2",
-                "p2s",
-                "p2e",
-                "type",
-                "id",
-                "f1",
-                "f2",
-                "f3",
-                "f4",
-                "f5",
-                "f6",
-              ],
-              separator: "\t",
-              genomicFieldsToConvert: [
-                { chromosomeField: "chr1", genomicFields: ["p1s", "p1e"] },
-                { chromosomeField: "chr2", genomicFields: ["p2s", "p2e"] },
-              ],
-            },
-            dataTransform: [
-              {
-                type: "filter",
-                field: "chr1",
-                oneOf: ["1", "16", "14", "9", "6", "5", "3"],
-              },
-              {
-                type: "filter",
-                field: "chr2",
-                oneOf: ["1", "16", "14", "9", "6", "5", "3"],
-              },
-            ],
-            mark: "withinLink",
-            x: { field: "p1s", type: "genomic" },
-            xe: { field: "p1e", type: "genomic" },
-            x1: { field: "p2s", type: "genomic" },
-            x1e: { field: "p2e", type: "genomic" },
-            stroke: {
-              field: "type",
-              type: "nominal",
-              domain: [
-                "deletion",
-                "inversion",
-                "translocation",
-                "tandem-duplication",
-              ],
-            },
-            strokeWidth: { value: 0.8 },
-            opacity: { value: 0.15 },
-            width,
-            height: 100,
-          },
-        ],
+		"tracks": [
+			{
+			  "data": {
+				"url": "https://raw.githubusercontent.com/armtsf/tmp/main/matches-copy.csv",
+				"type": "csv",
+				"separator": "\t",
+				"chromosomeField": "CHROM",
+				"genomicFields": ["POS"]
+			  },
+			  "mark": "rect",
+			  "color": {
+				"field": "Type",
+				"type": "nominal",
+				"domain": [
+				  "Insertion",
+				  "Deletion"
+				],
+				"range": [
+				  "black",
+				  "red"
+				]
+			  },
+			  "x": {"field": "POS", "type": "genomic"},
+			  // "xe": {"field": "p1e", "type": "genomic"},
+			  "stroke": {"value": "lightgray"},
+			  "strokeWidth": {"value": 0.5},
+			  "width": 700,
+			  "height": 30
+			},
+			{
+			  "data": {
+				"url": "https://raw.githubusercontent.com/armtsf/tmp/main/matches-copy.csv",
+				"type": "csv",
+				"separator": "\t",
+				"chromosomeField": "CHROM",
+				"genomicFields": ["POS"]
+			  },
+			  "mark": "rect",
+			  "color": {
+				"field": "ClinicalSignificance",
+				"type": "nominal",
+				"domain": [
+				  "Benign",
+				  "Uncertain significance",
+				  "Pathogenic",
+				  "Likely pathogenic"
+				],
+				"range": [
+				  "green",
+				  "blue",
+				  "yellow",
+				  "red"
+				]
+			  },
+			  "x": {"field": "POS", "type": "genomic"},
+			  // "xe": {"field": "p1e", "type": "genomic"},
+			  "stroke": {"value": "lightgray"},
+			  "strokeWidth": {"value": 0.5},
+			  "width": 700,
+			  "height": 30
+			},
+			{
+				  "data": {
+					"type": "csv",
+					"url": "https://raw.githubusercontent.com/armtsf/tmp/main/matched-new.csv",
+					"separator": "\t",
+					"genomicFieldsToConvert": [
+					  {"chromosomeField": "chr1", "genomicFields": ["p1s", "p1e"]},
+					  {"chromosomeField": "chr2", "genomicFields": ["p2s", "p2e"]}
+					]
+				  },
+				  // "dataTransform": [
+				  //   {
+				  //     "type": "filter",
+				  //     "field": "chr1",
+				  //     "oneOf": ["1", "16", "14", "9", "6", "5", "3"]
+				  //   },
+				  //   {
+				  //     "type": "filter",
+				  //     "field": "chr2",
+				  //     "oneOf": ["1", "16", "14", "9", "6", "5", "3"]
+				  //   }
+				  // ],
+				  "mark": "withinLink",
+				  "x": {"field": "p1s", "type": "genomic"},
+				  "xe": {"field": "p1e", "type": "genomic"},
+				  "x1": {"field": "p2s", "type": "genomic"},
+				  "x1e": {"field": "p2e", "type": "genomic"},
+				  "stroke": {
+					"field": "type",
+					"type": "nominal",
+					"domain": [
+					  "Deletion",
+					  "Inversion",
+					  "Translocation",
+					  "tandem-duplication",
+					  "Insertion"
+					]
+				  },
+				  "strokeWidth": {"value": 0.8},
+				  "opacity": {"value": 0.15},
+				  "width": 500,
+				  "height": 100
+		  }
+		  ],
       },
       {
         spacing: 10,
@@ -119,7 +129,7 @@ const GenomePlots = ({ width }) => {
                   row: "sample",
                   column: "position",
                   value: "peak",
-                  categories: ["sample 1", "sample 2", "sample 3", "sample 4"],
+                  categories: ["Uncertain Significance", "Benign", "Likely Pathogenic", "Pathogenic"],
                   binSize: 4,
                 },
                 mark: "bar",
@@ -150,7 +160,7 @@ const GenomePlots = ({ width }) => {
                   row: "sample",
                   column: "position",
                   value: "peak",
-                  categories: ["sample 1", "sample 2", "sample 3", "sample 4"],
+                  categories: ["Uncertain Significance", "Benign", "Likely Pathogenic", "Pathogenic"],
                   binSize: 4,
                 },
                 mark: "bar",
